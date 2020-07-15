faa18bdc249d4ad9858a1a5d427990793b1a7e28
Elena <helena.liliya@gmail.com>
2020-07-15 16:45:00 +0300

    Added Security

--- tabula/src/main/java/bg/softuni/tabula/users/repository/UserRepository.java
+++ tabula/src/main/java/bg/softuni/tabula/users/repository/UserRepository.java
@@ -4,6 +4,9 @@ import bg.softuni.tabula.users.model.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<UserEntity, Long> {
+    Optional<UserEntity> findByEmail(String username);
 }
