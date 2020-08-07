f4d250cb99dc55ba420e66c7b8ed29b97b7144f2
Elena <helena.liliya@gmail.com>
2020-08-07 14:08:42 +0300

    Initial commit

--- aop/src/main/java/bg/softuni/aop/basic/BasicPointcutSample.java
+++ aop/src/main/java/bg/softuni/aop/basic/BasicPointcutSample.java
@@ -1,24 +1,30 @@
 package bg.softuni.aop.basic;
 
-import org.aspectj.lang.annotation.After;
-import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
-import org.aspectj.lang.annotation.Pointcut;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.*;
 import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
 public class BasicPointcutSample {
-    @Pointcut("execution(* bg.softuni.aop.model.Student.* (..))")
+    @Pointcut("execution(* bg.softuni.aop.model.Student.sayHello())")
     public void trackSayHello() { }
 
-    @Before("trackSayHello()")
+    //@Before("trackSayHello()")
     public void logBeforeMethod() {
         System.out.println("Before executing of say hello!");
     }
 
-    @After("trackSayHello()")
+    //@After("trackSayHello()")
     public void logAfterMethod() {
         System.out.println("After executing of say hello!");
+    }
+
+    @Around("trackSayHello()")
+    public Object logAroundMethodExecution(ProceedingJoinPoint pjp) throws Throwable {
+        System.out.println("Before execution");
+        Object ret = pjp.proceed();
+        System.out.println("After execution");
+        return ret;
     }
 }
