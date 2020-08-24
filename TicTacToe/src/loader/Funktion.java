bb47377f5813ad255b185ff3d0bdb8f1f313f45d
Elena <helena.liliya@gmail.com>
2020-08-24 16:28:22 +0300

    Better Version of Tic Tac Toe game - first commit!

--- TicTacToe/src/loader/Funktion.java
+++ TicTacToe/src/loader/Funktion.java
@@ -1,2 +1,12 @@
-package loader;public class Funktion {
+package loader;
+
+public class Funktion {
+    public static void reset() {
+        for (int i = 0; i < Gui.state.length; i++) {
+            Gui.state[i] = 0;
+        }
+
+        Gui.player = 0;
+        Gui.winner = 0;
+    }
 }
