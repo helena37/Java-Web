e97bfbc5a024893660a8226dcad8140ad97fe581
e.hristoskova <e.hristoskova@icloud.com>
2021-07-30 17:52:13 +0300

    Added Security

--- userservice/src/main/java/bg/hristoskova/userservice/domain/User.java
+++ userservice/src/main/java/bg/hristoskova/userservice/domain/User.java
@@ -11,7 +11,10 @@ import java.util.Collection;
 import static javax.persistence.FetchType.*;
 import static javax.persistence.GenerationType.*;
 
-@Entity @Data @NoArgsConstructor @AllArgsConstructor
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class User {
     @Id @GeneratedValue(strategy = AUTO)
     private Long id;
