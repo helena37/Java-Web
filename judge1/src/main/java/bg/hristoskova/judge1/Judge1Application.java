89e733e2b456ef13a1172e6509ef3adff2cff664
e.hristoskova <e.hristoskova@icloud.com>
2021-11-08 15:57:31 +0200

    Initial commit

---
+++ judge1/src/main/java/bg/hristoskova/judge1/Judge1Application.java
@@ -0,0 +1,13 @@
+package bg.hristoskova.judge1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Judge1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Judge1Application.class, args);
+	}
+
+}
---
+++ judge1/src/test/java/bg/hristoskova/judge1/Judge1ApplicationTests.java
@@ -0,0 +1,13 @@
+package bg.hristoskova.judge1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Judge1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
