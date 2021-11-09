45d929ce0eebc7cf4c691b55bb4bacdb075e0b7f
e.hristoskova <e.hristoskova@icloud.com>
2021-11-09 12:42:25 +0200

    Database configured

---
+++ judge1/src/main/java/bg/hristoskova/judge1/model/entity/Exercise.java
@@ -0,0 +1,7 @@
+package bg.hristoskova.judge1.model.entity;/**
+* @author helena81
+* @version 1.0
+* @since 8.11.21
+*/
+public class Exercise {
+}
---
+++ judge1/src/main/java/bg/hristoskova/judge1/repository/UserRepository.java
@@ -0,0 +1,7 @@
+package bg.hristoskova.judge1.repository;/**
+* @author helena81
+* @version 1.0
+* @since 9.11.21
+*/
+public interface UserRepository {
+}
