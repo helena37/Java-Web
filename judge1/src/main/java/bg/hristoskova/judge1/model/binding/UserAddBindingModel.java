2b09307fc9f3c47641035d964836809b74eae2ac
e.hristoskova <e.hristoskova@icloud.com>
2021-11-10 11:01:41 +0200

    Entities/Some Controllers

--- judge1/src/main/java/bg/hristoskova/judge1/model/binding/UserAddBindingModel.java
+++ judge1/src/main/java/bg/hristoskova/judge1/model/binding/UserAddBindingModel.java
@@ -1,7 +1,70 @@
-package bg.hristoskova.judge1.model.binding;/**
-* @author helena81
-* @version 1.0
-* @since 9.11.21
-*/
+package bg.hristoskova.judge1.model.binding;
+
+import org.hibernate.validator.constraints.Length;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+
+/**
+ * @author helena81
+ * @version 1.0
+ * @since 9.11.21
+ */
 public class UserAddBindingModel {
+    private String username;
+    private String password;
+    private String confirmPassword;
+    private String email;
+    private String git;
+
+    public UserAddBindingModel() {
+    }
+
+    @Length(min = 2, max = 10, message = "Username should be at least 2 and maximal 10 characters!")
+    @NotNull
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    @Length(min = 2, message = "Password should be at least 2 characters!")
+    @NotNull
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getConfirmPassword() {
+        return confirmPassword;
+    }
+
+    public void setConfirmPassword(String confirmPassword) {
+        this.confirmPassword = confirmPassword;
+    }
+
+    @Email
+    @NotNull
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Pattern(regexp = "https:\\/\\/github\\.com\\/.+", message = "Enter, valid gitHub address!")
+    public String getGit() {
+        return git;
+    }
+
+    public void setGit(String git) {
+        this.git = git;
+    }
 }
--- judge1/src/main/java/bg/hristoskova/judge1/web/UserController.java
+++ judge1/src/main/java/bg/hristoskova/judge1/web/UserController.java
@@ -1,8 +1,16 @@
 package bg.hristoskova.judge1.web;
 
+import bg.hristoskova.judge1.model.binding.UserAddBindingModel;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import javax.validation.Valid;
 
 /**
  * @author helena81
@@ -16,5 +24,25 @@ public class UserController {
     @GetMapping("/login")
     public String login() {
         return "login";
+    }
+
+    @GetMapping("/register")
+    public String register() {
+        return "register";
+    }
+
+    @PostMapping("/register")
+    public ModelAndView registerConfirm(@Valid @ModelAttribute("userAddBindingModel")
+                                                UserAddBindingModel userAddBindingModel,
+                                        BindingResult bindingResult,
+                                        ModelAndView modelAndView,
+                                        RedirectAttributes redirectAttributes) {
+        if (bindingResult.hasErrors()) {
+            modelAndView.setViewName("redirect:/users/register");
+        } else {
+            modelAndView.setViewName("redirect:/users/login");
+        }
+
+        return modelAndView;
     }
 }
