701523a5fe6bc51eab4b7b3c9ee029a9a57122ff
Elena Hristoskova <e.hristoskova@icloud.com>
2022-05-12 15:52:12 +0300

    Initial commit

---
+++ functional-programming/src/main/java/imperative/Main.java
@@ -0,0 +1,57 @@
+package imperative;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static imperative.Main.Gender.FEMALE;
+import static imperative.Main.Gender.MALE;
+
+/**
+ * @author helena81
+ * @version 1.0
+ * @since 12.05.22
+ */
+public class Main {
+    public static void main(String[] args) {
+        List<Person> people = List.of(
+                new Person("John", MALE),
+                new Person("Maria", FEMALE),
+                new Person("Aisha", FEMALE),
+                new Person("Alex", MALE),
+                new Person("Alice", FEMALE)
+        );
+
+        //Imperative approach
+        List<Person> females = new ArrayList<>();
+
+        for (Person female : people) {
+            if (FEMALE.equals(female.gender)) {
+                females.add(female);
+            }
+        }
+
+        for (Person female : females) {
+            System.out.println(female.toString());
+        }
+    }
+
+    static class Person {
+        private final String name;
+        private final Gender gender;
+
+        Person(String name, Gender gender) {
+            this.name = name;
+            this.gender = gender;
+        }
+
+        @Override
+        public String toString() {
+            return String.format("name: %s%ngender: %s%n",
+                    this.name, this.gender);
+        }
+    }
+
+    enum Gender {
+        MALE, FEMALE
+    }
+}
